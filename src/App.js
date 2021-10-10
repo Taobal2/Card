7125a1a6a6c56050bd5d06881e8cbf2838488fef
Balogun Taoheed <baloguntaoheed2@gmail.com>
2021-10-10 16:42:31 -0700

    a card work

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "antd/dist/antd.css";
+import {Card} from "./Component/Practice Card/Card";
+
+const App = () =>{
+
+    return(
+        <div>
+            <Card/>
+        </div>
+
+    )
+
+}
+
+export default App
